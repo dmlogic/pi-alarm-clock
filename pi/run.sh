89f16e02c61926a41e00128020100111c6944036
Darren Miller <darren@dmlogic.net>
2021-10-19 18:24:47 +0100

    Incognito mode to avoid browser cache

--- pi/run.sh
+++ pi/run.sh
@@ -15,4 +15,4 @@ xset s 10
 unclutter -idle 0 &
 
 # Start the app, with audio playback allowed and in kiosk mode
-chromium-browser --app=http://127.0.0.1 --autoplay-policy=no-user-gesture-required --kiosk
+chromium-browser --app=http://127.0.0.1 --incognito --autoplay-policy=no-user-gesture-required --kiosk
