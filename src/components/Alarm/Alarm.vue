f8077f306c13960180bc59bdcd485c5f1c38005e
Darren Miller <darren@dmlogic.net>
2021-11-13 15:17:28 +0000

    Split alarm settings into separate component

--- src/components/Alarm/Alarm.vue
+++ src/components/Alarm/Alarm.vue
@@ -1,8 +1,8 @@
 <script>
 import { defineComponent } from 'vue'
 import Modal from './Modal.vue'
-import NumberInput from './NumberInput.vue'
 import Button from './Button.vue'
+import Settings from './Settings.vue'
 import { padTime } from '../../functions.js'
 export default defineComponent({
     data() {
@@ -31,18 +31,15 @@ export default defineComponent({
              * How many times we've snoozed
              */
             snoozeCount: 0,
-            /**
-             * Are we showing the settings panel?
-             */
-            displaySettings: false,
+
             /**
              * The sound we will play
              */
             audioFile: new Audio(this.$store.state.audioFile),
             /**
-             * The days set to sound the alarm
+             * Are settings being displayed?
              */
-            daySelections: [false, false, false, false, false, false, false],
+            displaySettings: false,
         }
     },
     methods: {
@@ -120,39 +117,6 @@ export default defineComponent({
                 this.$store.state.alarmDays.includes(triggerDay)
             )
         },
-        setHour(to) {
-            this.$store.commit('setAlarmHour', to)
-        },
-        setMinute(to) {
-            this.$store.commit('setAlarmMinute', to)
-        },
-        setSnoozes(to) {
-            this.$store.commit('setSnoozes', to)
-        },
-        setSnoozeMinutes(to) {
-            this.$store.commit('setSnoozeMinutes', to)
-        },
-        setVolume(to) {
-            this.$store.commit('setVolume', to)
-            const audioPreview = new Audio('/audio/beep-beep.mp3')
-            audioPreview.volume = parseInt(to) / 10
-            audioPreview.play()
-            setTimeout(function () {
-                audioPreview.pause()
-            }, 1000)
-        },
-        setDays() {
-            let selection = []
-            for (let index = 0; index < this.daySelections.length; index++) {
-                if (this.daySelections[index] === true) {
-                    selection.push(index)
-                }
-            }
-            this.$store.commit('setAlarmDays', selection)
-        },
-        setOnOff(ev) {
-            this.$store.commit('setOnOff', ev.target.checked)
-        },
     },
     computed: {
         bellClass() {
@@ -170,15 +134,12 @@ export default defineComponent({
         },
     },
     mounted() {
-        this.$store.state.alarmDays.forEach(
-            (i) => (this.daySelections[i] = true)
-        )
         this.check()
     },
     components: {
         Modal,
-        NumberInput,
         Button,
+        Settings,
     },
 })
 </script>
@@ -253,154 +214,10 @@ export default defineComponent({
             </Modal>
         </section>
 
-        <section class="settings" v-if="displaySettings">
-            <Modal>
-                <template v-slot:body>
-                    <label class="font-bold text-2xl block mb-6"
-                        ><input
-                            type="checkbox"
-                            class="form-checkbox rounded p-3"
-                            value="1"
-                            :checked="$store.state.alarmIsSet"
-                            @change="setOnOff"
-                        />
-                        Alarm on</label
-                    >
-                    <div
-                        class="
-                            mb-6
-                            grid grid-flow-col grid-cols-3 grid-rows-1
-                            gap-4
-                        "
-                    >
-                        <NumberInput
-                            :label="'Hour'"
-                            :value="$store.state.alarmHour"
-                            :max="23"
-                            @change="setHour"
-                        />
-                        <NumberInput
-                            :label="'Minute'"
-                            :value="$store.state.alarmMinute"
-                            :max="59"
-                            @change="setMinute"
-                        />
-                    </div>
-
-                    <div
-                        class="
-                            mb-6
-                            grid grid-flow-col grid-cols-7 grid-rows-1
-                            gap-1
-                            text-xl
-                            font-bold
-                        "
-                    >
-                        <label
-                            ><input
-                                type="checkbox"
-                                class="form-checkbox rounded p-3"
-                                value="1"
-                                v-model="daySelections[1]"
-                                @change="setDays"
-                            />
-                            Mon</label
-                        >
-                        <label
-                            ><input
-                                type="checkbox"
-                                class="form-checkbox rounded p-3"
-                                value="2"
-                                v-model="daySelections[2]"
-                                @change="setDays"
-                            />
-                            Tue</label
-                        >
-                        <label
-                            ><input
-                                type="checkbox"
-                                class="form-checkbox rounded p-3"
-                                value="3"
-                                v-model="daySelections[3]"
-                                @change="setDays"
-                            />
-                            Wed</label
-                        >
-                        <label
-                            ><input
-                                type="checkbox"
-                                class="form-checkbox rounded p-3"
-                                value="4"
-                                v-model="daySelections[4]"
-                                @change="setDays"
-                            />
-                            Thu</label
-                        >
-                        <label
-                            ><input
-                                type="checkbox"
-                                class="form-checkbox rounded p-3"
-                                value="5"
-                                v-model="daySelections[5]"
-                                @change="setDays"
-                            />
-                            Fri</label
-                        >
-                        <label
-                            ><input
-                                type="checkbox"
-                                class="form-checkbox rounded p-3"
-                                value="6"
-                                v-model="daySelections[6]"
-                                @change="setDays"
-                            />
-                            Sat</label
-                        >
-                        <label
-                            ><input
-                                type="checkbox"
-                                class="form-checkbox rounded p-3"
-                                value="0"
-                                v-model="daySelections[0]"
-                                @change="setDays"
-                            />
-                            Sun</label
-                        >
-                    </div>
-                    <div
-                        class="grid grid-flow-col grid-cols-3 grid-rows-1 gap-4"
-                    >
-                        <NumberInput
-                            :label="'Snoozes'"
-                            :value="$store.state.snoozesAllowed"
-                            :max="10"
-                            :min="1"
-                            @change="setSnoozes"
-                        />
-                        <NumberInput
-                            :label="'Snooze minutes'"
-                            :value="$store.state.snoozeMinutes"
-                            :max="10"
-                            :min="5"
-                            @change="setSnoozeMinutes"
-                        />
-                        <NumberInput
-                            :label="'Volume'"
-                            :value="$store.state.volume"
-                            :max="10"
-                            :min="1"
-                            @change="setVolume"
-                        />
-                    </div>
-                </template>
-                <template v-slot:footer>
-                    <Button
-                        :label="'OK'"
-                        v-on:click="displaySettings = false"
-                    />
-                </template>
-            </Modal>
-        </section>
+        <Settings
+            v-if="displaySettings"
+            v-on:close="displaySettings = false"
+        />
 
         <section class="alert snooze" v-if="isSnoozed">
             <Modal :mt="'mt-36'">
---
+++ src/components/Alarm/Settings.vue
@@ -0,0 +1,215 @@
+<script>
+import { defineComponent } from 'vue'
+import Modal from './Modal.vue'
+import NumberInput from './NumberInput.vue'
+import Button from './Button.vue'
+export default defineComponent({
+    data() {
+        return {
+            daySelections: [false, false, false, false, false, false, false],
+        }
+    },
+    methods: {
+        setHour(to) {
+            this.$store.commit('setAlarmHour', to)
+        },
+        setMinute(to) {
+            this.$store.commit('setAlarmMinute', to)
+        },
+        setSnoozes(to) {
+            this.$store.commit('setSnoozes', to)
+        },
+        setSnoozeMinutes(to) {
+            this.$store.commit('setSnoozeMinutes', to)
+        },
+        setVolume(to) {
+            this.$store.commit('setVolume', to)
+            const audioPreview = new Audio('/audio/beep-beep.mp3')
+            audioPreview.volume = parseInt(to) / 10
+            audioPreview.play()
+            setTimeout(function () {
+                audioPreview.pause()
+            }, 1000)
+        },
+        setDays() {
+            let selection = []
+            for (let index = 0; index < this.daySelections.length; index++) {
+                if (this.daySelections[index] === true) {
+                    selection.push(index)
+                }
+            }
+            this.$store.commit('setAlarmDays', selection)
+        },
+        setOnOff(ev) {
+            this.$store.commit('setOnOff', ev.target.checked)
+        },
+    },
+    mounted() {
+        this.$store.state.alarmDays.forEach(
+            (i) => (this.daySelections[i] = true)
+        )
+    },
+    components: {
+        Modal,
+        NumberInput,
+        Button,
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
+
+<template>
+
+        <section class="settings">
+            <Modal>
+                <template v-slot:body>
+                    <label class="font-bold text-2xl block mb-6"
+                        ><input
+                            type="checkbox"
+                            class="form-checkbox rounded p-3"
+                            value="1"
+                            :checked="$store.state.alarmIsSet"
+                            @change="setOnOff"
+                        />
+                        Alarm on</label
+                    >
+                    <div
+                        class="
+                            mb-6
+                            grid grid-flow-col grid-cols-3 grid-rows-1
+                            gap-4
+                        "
+                    >
+                        <NumberInput
+                            :label="'Hour'"
+                            :value="$store.state.alarmHour"
+                            :max="23"
+                            @change="setHour"
+                        />
+                        <NumberInput
+                            :label="'Minute'"
+                            :value="$store.state.alarmMinute"
+                            :max="59"
+                            @change="setMinute"
+                        />
+                    </div>
+
+                    <div
+                        class="
+                            mb-6
+                            grid grid-flow-col grid-cols-7 grid-rows-1
+                            gap-1
+                            text-xl
+                            font-bold
+                        "
+                    >
+                        <label
+                            ><input
+                                type="checkbox"
+                                class="form-checkbox rounded p-3"
+                                value="1"
+                                v-model="daySelections[1]"
+                                @change="setDays"
+                            />
+                            Mon</label
+                        >
+                        <label
+                            ><input
+                                type="checkbox"
+                                class="form-checkbox rounded p-3"
+                                value="2"
+                                v-model="daySelections[2]"
+                                @change="setDays"
+                            />
+                            Tue</label
+                        >
+                        <label
+                            ><input
+                                type="checkbox"
+                                class="form-checkbox rounded p-3"
+                                value="3"
+                                v-model="daySelections[3]"
+                                @change="setDays"
+                            />
+                            Wed</label
+                        >
+                        <label
+                            ><input
+                                type="checkbox"
+                                class="form-checkbox rounded p-3"
+                                value="4"
+                                v-model="daySelections[4]"
+                                @change="setDays"
+                            />
+                            Thu</label
+                        >
+                        <label
+                            ><input
+                                type="checkbox"
+                                class="form-checkbox rounded p-3"
+                                value="5"
+                                v-model="daySelections[5]"
+                                @change="setDays"
+                            />
+                            Fri</label
+                        >
+                        <label
+                            ><input
+                                type="checkbox"
+                                class="form-checkbox rounded p-3"
+                                value="6"
+                                v-model="daySelections[6]"
+                                @change="setDays"
+                            />
+                            Sat</label
+                        >
+                        <label
+                            ><input
+                                type="checkbox"
+                                class="form-checkbox rounded p-3"
+                                value="0"
+                                v-model="daySelections[0]"
+                                @change="setDays"
+                            />
+                            Sun</label
+                        >
+                    </div>
+                    <div
+                        class="grid grid-flow-col grid-cols-3 grid-rows-1 gap-4"
+                    >
+                        <NumberInput
+                            :label="'Snoozes'"
+                            :value="$store.state.snoozesAllowed"
+                            :max="10"
+                            :min="1"
+                            @change="setSnoozes"
+                        />
+                        <NumberInput
+                            :label="'Snooze minutes'"
+                            :value="$store.state.snoozeMinutes"
+                            :max="10"
+                            :min="5"
+                            @change="setSnoozeMinutes"
+                        />
+                        <NumberInput
+                            :label="'Volume'"
+                            :value="$store.state.volume"
+                            :max="10"
+                            :min="1"
+                            @change="setVolume"
+                        />
+                    </div>
+                </template>
+                <template v-slot:footer>
+                    <Button
+                        :label="'OK'"
+                        v-on:click="$emit('close')"
+                    />
+                </template>
+            </Modal>
+        </section>
+
+</template>
