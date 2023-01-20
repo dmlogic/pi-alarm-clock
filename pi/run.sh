be6539f76e758bef69a255814c2e1a02ad8d7393
Darren Miller <darren@darrenm.net>
2023-01-20 10:08:26 +0000

    Alternate approach to dodging cache

--- pi/run.sh
+++ pi/run.sh
@@ -7,5 +7,6 @@ xset s 10
 unclutter -idle 0 &
 
 # Start the app, with audio playback allowed and in kiosk mode
-chromium-browser --incognito --app=http://127.0.0.1 --autoplay-policy=no-user-gesture-required --kiosk
+#chromium-browser --incognito --app=http://127.0.0.1 --autoplay-policy=no-user-gesture-required --kiosk
+chromium-browser --new-window --aggressive-cache-discard --autoplay-policy=no-user-gesture-required --kiosk  --disk-cache-size=0 http://127.0.0.1
 
